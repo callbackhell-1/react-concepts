851dbbd35f358ef5b0499aae65f99932abc84799
Adarsh <jha01adarsh@gmail.com>
2024-01-03 02:35:19 +0530

    created nested element

--- 01_inception/App.js
+++ 01_inception/App.js
@@ -1,23 +1,23 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world from React !"
+/*
+<div id="parent">
+  <div id="child">
+    <h1>I'm h1 tag</h1>
+  </div>
+</div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm h1 tag")
+  )
 );
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
-console.log(heading); //object
-/**
- * heading is object not a h1 tag, ,
- * we are creating react element
- * h1 is react element
- * If we expand the console , it has props which contain child & attributes
- * props : { id: "heading", xyz: "abc" } + "Hello world from React !"
- * children -> 3rd argument in createElement.
- * attributes(heading,xyz)
- * React.createElement create a just an js obj., not actual h1 tag
- *
- *root.render(heading); -> rendor method, converts react element which is an object into html tag ie, <h1>...</h1> and render it to root element in html file
- */
+root.render(parent);
