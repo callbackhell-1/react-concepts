e86fbb217dd0dfb93726b521b23c38d34753da47
Adarsh <jha01adarsh@gmail.com>
2024-01-08 01:49:54 +0530

    importing react from node_modules

--- 02_ignite_our_app/App.js
+++ 02_ignite_our_app/App.js
@@ -1,32 +1,17 @@
-/*
-<div id="parent">
-  <div id="child">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-  </div>
-    <div id="child2">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-  </div>
-</div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm h1 tag"),
-      React.createElement("h2", {}, "I'm h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm h1 tag"),
-      React.createElement("h2", {}, "I'm h2 tag"),
-    ]),
-  ]);
-  
-  console.log(parent);
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
-  
-  //code is so complicated. so to avoid this we have jsx
-  
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
