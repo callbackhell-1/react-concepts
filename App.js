fb5795bd120ae1f9d6b226dd84ae0420dbba3512
Adarsh <jha01adarsh@gmail.com>
2024-01-13 23:32:45 +0530

    render component in another component - component composition

--- App.js
+++ App.js
@@ -2,15 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React element
-const jsxHeading = (
+const Heading = (
   <h1 className="hero-heading" tabIndex="2">
     Hello from JSX 👋
   </h1>
 );
 
+//another component
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+//another component
+const Title = () => (
+  <div>
+    <h1>This is my title component 📗</h1>
+  </div>
+);
+
 //functional components
 const HeadingComponent = () => (
   <div id="hero-heading">
+    {/* component composition  || render another component*/}
+    <Navbar />
+    <Title />
     <h1 id="heading">Hello from react functional component</h1>
   </div>
 );
