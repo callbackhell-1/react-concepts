a8c48a5679e179649a420d10b5bb675e66024d97
Adarsh <jha01adarsh@gmail.com>
2024-01-13 21:19:14 +0530

    functional components -2 ways to write it

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React element
 const jsxHeading = (
   <h1 className="hero-heading" tabIndex="2">
     Hello from JSX 👋
   </h1>
+);
+
+//functional components
+// 1st way :
+const HeadingComponents = () => {
+  return <h1>Hello from 1st functional component</h1>;
+};
+
+// 2nd way :
+const HeadingComponents2 = () => (
+  <h1>Hello from 2nd react functionalcomponent</h1>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
