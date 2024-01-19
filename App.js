9580e3fc035dff452ba10628e4209d72e4dbb21d
Adarsh <jha01adarsh@gmail.com>
2024-01-19 20:31:15 +0530

    UI mock & low lvl planning

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * Header
+    - logo 
+    - nav items
+ * body
+    - search input
+       -restraunt container
+          - restraunt card
+ * footer
+    - Links
+    - Address
+    - contact us
+ */
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
