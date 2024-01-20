622fa08a0eae0aaf60991c58f3149a2cb90f3f24
Adarsh <jha01adarsh@gmail.com>
2024-01-21 04:52:01 +0530

    multiple rest card

--- App.js
+++ App.js
@@ -64,6 +64,13 @@ const Body = () => {
         <div className="search">search</div>
         <div className="res-container">
           <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
         </div>
       </div>
     </div>
