c02d01482933e18c0f220d41379c8730ff8f1780
Adarsh <jha01adarsh@gmail.com>
2024-01-21 19:14:23 +0530

    took kfc data as an obj (45-168),then pass it to RestrauntCard omp as props(92) , & in ResCard we are consuming it (70-82)

--- App.js
+++ App.js
@@ -42,18 +42,143 @@ const Header = () => {
   );
 };
 
+const resObj = {
+  type: "restaurant",
+  data: {
+    type: "F",
+    id: "334475",
+    name: "KFC",
+    uuid: "eaed0e3b-7c0e-4367-8f59-f41d309fb93a",
+    city: "1",
+    area: "BTM Layout",
+    totalRatingsString: "500+ ratings",
+    cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    tags: [],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    deliveryTime: 36,
+    minDeliveryTime: 36,
+    maxDeliveryTime: 36,
+    slaString: "36 MINS",
+    lastMileTravel: 3.5,
+    slugs: {
+      restaurant: "kfc-btm-layout-btm",
+      city: "bangalore",
+    },
+    cityState: "1",
+    address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
+    locality: "2nd Stage",
+    parentId: 547,
+    unserviceable: false,
+    veg: false,
+    select: false,
+    favorite: false,
+    tradeCampaignHeaders: [],
+    aggregatedDiscountInfo: {
+      header: "FREE DELIVERY",
+      shortDescriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      subHeader: "",
+      headerType: 0,
+      superFreedel: "",
+    },
+    aggregatedDiscountInfoV2: {
+      header: "",
+      shortDescriptionList: [
+        {
+          meta: "Free Delivery",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      subHeader: "",
+      headerType: 0,
+      superFreedel: "",
+    },
+    ribbon: [
+      {
+        type: "PROMOTED",
+      },
+    ],
+    chain: [],
+    feeDetails: {
+      fees: [],
+      totalFees: 0,
+      message: "",
+      title: "",
+      amount: "",
+      icon: "",
+    },
+    availability: {
+      opened: true,
+      nextOpenMessage: "",
+      nextCloseMessage: "",
+    },
+    longDistanceEnabled: 0,
+    rainMode: "NONE",
+    thirdPartyAddress: false,
+    thirdPartyVendor: "",
+    adTrackingID: "cid=6109309~p=1~eid=00000186-a341-249f-05e6-09c500910178",
+    badges: {
+      imageBased: [],
+      textBased: [],
+      textExtendedBadges: [],
+    },
+    lastMileTravelString: "3.5 kms",
+    hasSurge: false,
+    sla: {
+      restaurantId: "334475",
+      deliveryTime: 36,
+      minDeliveryTime: 36,
+      maxDeliveryTime: 36,
+      lastMileTravel: 3.5,
+      lastMileDistance: 0,
+      serviceability: "SERVICEABLE",
+      rainMode: "NONE",
+      longDistance: "NOT_LONG_DISTANCE",
+      preferentialService: false,
+      iconType: "EMPTY",
+    },
+    promoted: true,
+    avgRating: "3.8",
+    totalRatings: 500,
+    new: false,
+  },
+  subtype: "basic",
+};
 const RestaurantCard = (props) => {
-  const { resName, cuisine } = props;
+  const { resData } = props;
+  console.log(resData);
   return (
     <div className="res-card">
       <img
         className="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gt1qlbsgaklbzlg0olga"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bdcd233971b7c81bf77e1fa4471280eb"
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>30 minutes</h4>
+      <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisines.join(",")}</h4>
+      <h4>{resData.data.avgRating} star</h4>
+      <h4>{resData.data.deliveryTime} Minutes</h4>
     </div>
   );
 };
@@ -64,8 +189,7 @@ const Body = () => {
       <div>
         <div className="search">search</div>
         <div className="res-container">
-          <RestaurantCard resName="Megna Food" cuisine="North Indian" />
-          <RestaurantCard resName="KFC" cuisine="Fast food" />
+          <RestaurantCard resData={resObj} />
         </div>
       </div>
     </div>
